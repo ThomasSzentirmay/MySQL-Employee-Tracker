ca3ad22396198e452c5fd0e1eeae427f5394a073
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-09 15:35:06 -0400

    started query file

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,16 @@
 USE employee_tracker
+
+-- Get all employees by role
+SELECT
+    s.first_name,
+    s.last_name,
+    c.title AS role_title,
+    u.name AS department
+    FROM employee e
+        JOIN roles r
+            ON e.roles_id = r.id
+        JOIN department d
+            ON r.department_id = d.id
+        WHERE r.id = 1;
+
+-- Get all employees
