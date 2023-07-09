b8c7c555082dbc20b018efc9142541a858c1e1d3
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-09 15:16:10 -0400

    seeded data into roles table

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary INT NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (university_id)
         REFERENCES department(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,17 @@
 USE employee_tracker;
 
 INSERT INTO department (department_name) VALUES
-    ('Sales Lead'),
-    ('Salesperson'),
-    ('Lead Engineer'),
-    ('Software Engineer'),
-    ('Account Manager'),
-    ('Accountant'),
-    ('Legal Team Lead'),
-    ('Lawyer');
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales');
+
+INSERT INTO roles (title, salary, department_id) VALUES
+    ('Sales Lead', 100000, 4),
+    ('Salesperson', 80000, 4),
+    ('Lead Engineer', 150000, 1),
+    ('Software Engineer', 120000, 1),
+    ('Account Manager', 160000, 2),
+    ('Accountant', 125000, 2),
+    ('Legal Team Lead', 250000, 3),
+    ('Lawyer', 190000, 3);
