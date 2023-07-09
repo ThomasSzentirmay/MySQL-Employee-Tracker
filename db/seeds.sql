89eee1b7506de3277c5f08ae24aa258bf7d52b07
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-09 15:11:17 -0400

    seeded data into department

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE employee_tracker;
+
+INSERT INTO department (department_name) VALUES
+    ('Sales Lead'),
+    ('Salesperson'),
+    ('Lead Engineer'),
+    ('Software Engineer'),
+    ('Account Manager'),
+    ('Accountant'),
+    ('Legal Team Lead'),
+    ('Lawyer');
